eade43b61c4ac9f6c1342a303d96aaca9bb2db79
Max Maischein <corion@corion.net>
2018-01-14 19:52:48 +0100

    Use function signatures

--- lib/IP/CloudHoster/AWS.pm
+++ lib/IP/CloudHoster/AWS.pm
@@ -1,5 +1,9 @@
 package IP::CloudHoster::AWS;
 use strict;
+use Filter::signatures;
+no warnings 'experimental::signatures';
+use feature 'signatures';
+
 use Moo 2;
 use Future;
 use Net::Netmask;
@@ -37,22 +41,21 @@ has '_aws_ip_ranges' => (
 # Also, can we turn the UA into a queueing resource with a similar
 # approach?
 
-sub retrieve_aws_ips {
-    my( $self, $address, $ua ) = @_;
-    
+sub retrieve_aws_ips( $self, $address, $ua ) {
+    # Should we canonicalize $address?
+
     # Only request the resource once, even if there are multiple calls
     # while the request is still outstanding
     shared_resource(\($self->inflight_requests->{ $address }))
     ->fetch( sub { $ua->http_get( $address ) })
 }
 
-sub ip_ranges_json {
-    my( $self, %options ) = @_;
+sub ip_ranges_json( $self, %options ) {
     my $res;
-    
+
     $options{ ua } ||= $self->ua;
     $options{ aws_ip_range_url } ||= $self->aws_ip_range_url;
-    
+
     my $r = $self->_aws_ip_ranges;
     if( $r ) {
         $res = Future->done( $r )
@@ -71,9 +74,7 @@ sub ip_ranges_json {
     $res
 }
 
-sub parse_ip_ranges {
-    my( $self, $json ) = @_;
-    
+sub parse_ip_ranges( $self, $json ) {
     my @ip_ranges;
     for my $e (@{ $json->{prefixes} }) {
         my $entry = {
@@ -81,18 +82,17 @@ sub parse_ip_ranges {
             provider => 'amazon',
             range => Net::Netmask->new( $e->{ ip_prefix } ),
         };
-        
+
         push @ip_ranges, $entry;
     };
-    
+
     $self->_aws_ip_ranges( $ip_ranges );
     return $self->_aws_ip_ranges();
 }
 
-sub ip_ranges {
-    my( $self, %options ) = @_;
+sub ip_ranges( $self, %options ) {
     my $res;
-    
+
     my $r = $self->_aws_ip_ranges;
     if( $r ) {
         $res = Future->done( $r )
@@ -110,18 +110,16 @@ sub ip_ranges {
     $res
 }
 
-sub identify {
-    my( $self, $ip, %options ) = @_;
-    
+sub identify( $self, $ip, %options ) {
     $self->ip_ranges( %options )->then(sub {
         my( $ip_ranges ) = @_;
-        
+
         for my $prefix (@$ip_ranges) {
             if( $prefix->{range}->match( $ip ) {
                 return $prefix
             };
         };
-        
+
         return()
     });
 }

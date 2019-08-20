eadbd316df06a19cc34f8c38412ccfc46f5ed1cb
Max Maischein <corion@corion.net>
2019-08-20 18:42:40 +0200

    Don't die on invalid reverse lookup

--- lib/IP/CloudHoster/GoogleBot.pm
+++ lib/IP/CloudHoster/GoogleBot.pm
@@ -24,17 +24,27 @@ sub reverse_lookup( $self, $address ) {
     # Only request the resource once, even if there are multiple calls
     # while the request is still outstanding
     shared_resource(\($self->inflight_requests->{ $address }))
-    ->fetch( sub { 
+    ->fetch( sub {
         my $f = AnyEvent::Future->new;
         #warn "Verifying $address";
-        AnyEvent::DNS::reverse_verify( $address, sub { warn "Verified @_"; $f->done(@_) } );
+        AnyEvent::DNS::reverse_verify( $address, sub {
+            if( $_[0] ) {
+                $f->done(@_)
+            } else {
+                $f->fail("Reverse DNS for $address not found", "reverse_dns" => $address)
+            }
+        });
         return $f
     })
     ->then( sub( @hostnames ) {
         #warn "Checking $_" for @hostnames;
         my @ok = grep { /(google\.com|googlebot\.com)$/i } @hostnames;
-        0+@ok or die "No Google domain name";
-        return Future->done( @ok )
+
+        if( 0+@ok ) {
+            return Future->done( @ok )
+        } else {
+            return Future->fail( "No Google domain name", "reverse_dns" => $address );
+        }
     })
 }
 

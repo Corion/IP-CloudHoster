fe8a9b4e4984a7b3b16e8f6365a0c357dc7604d2
Max Maischein <corion@corion.net>
2019-08-20 19:06:11 +0200

    HostEurope has multiple ASNs

--- lib/IP/CloudHoster/HostEurope.pm
+++ lib/IP/CloudHoster/HostEurope.pm
@@ -1,7 +1,12 @@
 package IP::CloudHoster::HostEurope;
 use Moo 2;
 
-use constant 'asn' => 'AS20773';
+use constant 'asn' => [
+    'AS8972',  # DE
+    'AS20773', # DE
+    'AS20738', # UK
+    'AS35329', # DE
+];
 use constant 'provider' => 'hosteurope';
 
 with 'IP::CloudHoster::Role::ASN';
